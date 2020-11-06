20d4b5b704b7ef55ce00d849d29b4322620cd419
Chih-Hsuan Yen <yan12125@gmail.com>
2020-11-06 15:54:49 +0800

    Avoid filename conflicts on case-insensitive filesystems

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(name="namcap",
 
 	py_modules=["namcap"],
 	packages = find_packages(),
-	scripts=["namcap", 'parsepkgbuild'],
+	scripts=["bin/namcap", 'bin/parsepkgbuild'],
 	test_suite = "Namcap.tests",
 	data_files=DATAFILES)
 
